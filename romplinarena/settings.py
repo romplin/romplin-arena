f7369c1f3b99441767e77ce98d12fa3d74213d79
romplin <realromplin@gmail.com>
2025-01-06 16:22:33 -0600

    forms

--- romplinarena/settings.py
+++ romplinarena/settings.py
@@ -136,6 +136,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Forms
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- romplinarena/views.py
+++ romplinarena/views.py
@@ -1,5 +1,19 @@
 # romplinarena/views.py
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def home(request):
     return render(request, 'home.html')
